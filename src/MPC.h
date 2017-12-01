d826dd29ebaa38de7186e42937d83180407e2aae
Paco Gorina <fgorina@gmail.com>
2017-12-01 13:54:23 +0100

    Initial Message

---
+++ src/MPC.h
@@ -0,0 +1,30 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+#include <cppad/cppad.hpp>
+#include <cppad/ipopt/solve.hpp>
+
+using namespace std;
+// TODO: Set the timestep length and duration
+
+
+class MPC {
+ public:
+    typedef CPPAD_TESTVECTOR(double) Dvector;
+   // place to return solution
+    CppAD::ipopt::solve_result<Dvector> solution;
+
+  MPC();
+
+  virtual ~MPC();
+    
+    
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+};
+
+#endif /* MPC_H */
